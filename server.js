c1a13e08dc99c870983f1d1985a59bfe38e4119c
khanmehadi <99681744+khanmehadi@users.noreply.github.com>
2023-01-31 19:38:07 -0500

    added starter server code (#6)

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.port || 3000;
+const path = require('path');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '/public/'))})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.....`)
+})
